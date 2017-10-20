08814e8267f4c9199eda5bc277f774ca01ff42de
Lewis Vine <lew.vine@gmail.com>
2017-10-20 15:28:49 +0000

    basic user profile added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def show
+    if current_user == nil
+      flash[:notice] = 'Please sign in before continuing'
+      redirect_to welcome_index_path
+    end
+    @user = User.find_by(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users
   get 'welcome/index'
 
-  root 'welcome#index'
+  root 'users#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
