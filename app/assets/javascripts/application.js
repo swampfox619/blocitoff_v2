8310f11023ce09b361b962fa64eae979aa385c89
Lewis Vine <lew.vine@gmail.com>
2017-10-22 20:00:49 +0000

    jQuery reorder working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,31 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+// $('.arrow-up').bind('ajax:success', function () {
+//     var saveThis = $(this).closest('item').prev();
+//     $(this).closest('item').prev().replaceWith(this);
+//     $(this.closest('item').replaceWith(saveThis);
+// })
+
+$(document).ready(function() {
+
+    $('body').on('click', '.fa.fa-angle-up', function () {
+        var formerPrevious = $(this).closest('.item').prev().html();
+        var formerNext = $(this).closest('.item').html();
+        $(this).closest('.item').prev().html(formerNext);
+        $(this).closest('.item').html(formerPrevious);
+    });
+    
+    $('body').on('click', '.fa.fa-angle-down', function () {
+        // var nextNumber = $(this).closest('.item').find(".number").innerHTML()
+        // var prevNumber = $(this).closest('.item').next().find(".number").innerHTML()
+        var formerNext = $(this).closest('.item').next().html();
+        var formerPrevious = $(this).closest('.item').html();
+        $(this).closest('.item').next().html(formerPrevious);
+        $(this).closest('.item').html(formerNext);
+        // $(this).closest('.item').next().child("number").innerHTML(nextNumber);
+        // $(this).closest('.item').child("number").innerHTML(prevNumber)
+    });
+
+});
